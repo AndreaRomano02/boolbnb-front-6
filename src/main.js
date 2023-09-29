46e4ca775dd381ac2b0ca077a0b82f9b32fed73a
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-09-29 16:07:28 +0200

    added SHOW (I have to set up responsive)

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from "./App.vue";
 import AppLoader from "./components/AppLoader.vue";
 import { router } from "./router";
 
+
+
+
 const app = createApp(App);
 app.component("AppLoader", AppLoader);
 app.use(router);
