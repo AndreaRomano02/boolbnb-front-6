5630b880a4bd2c22773f293f117a30d145107d94
masepe95 <marcosepe10@gmail.com>
2023-10-03 12:08:31 +0200

    input added in AppHero

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,12 +1,24 @@
 <script>
 export default {
     name: 'AppHero',
+    emits: ["startSearch"],
+    data() {
+        return {
+            searchText: "",
+        }
+    }
 }
 </script>
 
 <template>
     <div id="app-hero" ref="hero">
         <div class="hero-overlay"></div>
+    </div>
+    <div class="searchbar input-group mb-3">
+        <input type="text" v-model="searchText" class="form-control" placeholder="Cerca un appartamento"
+            aria-describedby="button-addon2">
+        <button @click="$emit('startSearch', searchText)" class="btn btn-primary" type="button"
+            id="button-addon2">Cerca</button>
     </div>
 </template>
 
@@ -26,5 +38,13 @@ export default {
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.2);
+}
+
+.searchbar {
+    position: absolute;
+    top: 33%;
+    left: 33%;
+    width: 600px;
+
 }
 </style>
