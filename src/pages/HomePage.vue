cc80ae8d40980491a8d376cbc491d85a1dc72d03
AndreaRomano02 <andy.romano2002@gmail.com>
2023-10-04 14:23:21 +0200

    Add filter and store apartments with city

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,6 @@
 <script>
 import axios from 'axios';
-const endpoint = 'http://localhost:8000/api/apartments';
+import { endpoint } from '../data';
 import AppLoader from '../components/AppLoader.vue';
 import ApartmentList from '../components/apartments/ApartmentList.vue';
 import { store } from "../data/store";
@@ -12,6 +12,7 @@ export default {
     components: { AppHeader, AppHero, ApartmentList, AppLoader, ApartmentCard },
     data() {
         return {
+            endpoint,
             store,
             isLoading: false,
             isHomePage: true,
@@ -20,21 +21,33 @@ export default {
             addressFilter: '',
         };
     },
+    created() {
+        this.fetchApartments();
+    },
     methods: {
+        fetchApartments() {
+            this.isLoading = true;
+            axios.get(endpoint).then((res) => {
+                this.store.apartments = res.data;
+            }).catch((err) => { console.error(err) })
+                .then(() => {
+                    this.isLoading = false;
+                })
+        },
         onAddressChange(input) {
             this.addressFilter = input;
             if (this.formSubmit) {
                 const params = {
                     params: {
                         city: this.addressFilter,
+
                     },
                 };
 
                 this.isLoading = true
                 axios.get(endpoint, params)
                     .then(res => {
-                        console.log(res.data)
-                        res.data = this.apartments
+                        this.apartments = res.data
                     })
                     .catch(error => {
                         console.error(error);
@@ -54,7 +67,8 @@ export default {
         <AppHero :apartments="store.apartments" @address-change="onAddressChange" @form-submit="formSubmit = true" />
         <AppLoader v-if="isLoading" />
         <div v-else class="pt-4">
-            <ApartmentList class="my-5" :apartments="store.apartments" />
+            <ApartmentList v-if="!apartments.length" class="my-5" :apartments="store.apartments" />
+            <ApartmentList v-else class="my-5" :apartments="apartments" />
         </div>
     </main>
 </template>
--- src/pages/apartments/ApartmentsPage.vue
+++ src/pages/apartments/ApartmentsPage.vue
@@ -1,4 +1,6 @@
 <script>
+import { endpoint } from '../../data';
+import axios from 'axios';
 import ApartmentList from '../../components/apartments/ApartmentList.vue';
 import AppHeader from '../../components/AppHeader.vue';
 import { store } from '../../data/store';
@@ -8,14 +10,32 @@ export default {
     components: { AppHeader, ApartmentList },
     data() {
         return {
+            endpoint,
             store,
             isHomePage: false,
+            isLoading: false,
         };
     },
+    methods: {
+
+        fetchApartments() {
+            this.isLoading = true;
+            axios.get(endpoint).then((res) => {
+                this.store.apartments = res.data;
+            }).catch((err) => { console.error(err) })
+                .then(() => {
+                    this.isLoading = false;
+                })
+        },
+    },
+    created() {
+        this.fetchApartments();
+    }
 };
 </script>
 
 <template>
+    <AppLoader v-if="isLoading" />
     <AppHeader :is-home-page="isHomePage" />
     <div class="pt-5 mt-5">
         <ApartmentList :apartments="store.apartments" />
