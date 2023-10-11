1e12770ec9ff9b489c26213669fe51c3afffa57c
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-10-11 20:12:40 +0200

    changed AppHero appearence

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -29,6 +29,7 @@ export default {
 <template>
     <div id="app-hero" ref="hero">
         <div class="hero-overlay d-flex align-items-center justify-content-center">
+            <h1 class="text-white">Experience new stories.</h1>
             <div class="search-input">
                 <AppSearchTerm @address-change="onAddressChange" @form-submit="$emit('form-submit')"
                     @distance-change="onDistanceChange" />
@@ -44,8 +45,8 @@ export default {
 
 #app-hero {
     height: 100vh;
-    background-image: url('/hero.jpg');
-    background-position: top;
+    background-image: url('/boreal.jpg');
+    background-position: center;
     background-size: cover;
     position: relative;
 }
@@ -59,9 +60,21 @@ export default {
     background-color: rgba(0, 0, 0, 0.5);
 }
 
+h1 {
+    text-align: center;
+    color: #ffffff22;
+    font-size: 1.5rem;
+    position: absolute;
+    top: 20%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+}
 
 .search-input {
     width: 50vw;
+
+
 }
 
 @media (max-width: 992px) {
